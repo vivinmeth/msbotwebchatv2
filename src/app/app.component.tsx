35cfcca027d55baecbd3c40ca8557d397cc5841d
VivinMeth <vivinmeth@gmail.com>
2021-08-20 23:07:58 +0530

    #2 in progress 2 added todo

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -3,6 +3,7 @@ import AppStyles from './app.module.scss';
 import {CoreComponent} from "./components/core/core.component";
 import {EmplayWebChatAPI} from "./apis";
 
+// ToDo: this probably must be a class with a functional react component.
 export function EmplayWebChat(props: {API?: EmplayWebChatAPI}) {
 
     const API = useMemo(() => props.API || new EmplayWebChatAPI(
